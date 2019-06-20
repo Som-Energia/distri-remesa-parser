32c47bfbf60ea5d682a16e5f5577183502462d27
Oriol Piera <oriol.piera@somenergia.coop>
2019-06-20 14:01:26 +0200

    Fix output file name and path

--- distriremesaparser/xlsparser.py
+++ distriremesaparser/xlsparser.py
@@ -20,14 +20,19 @@ class XLSParser:
         else:
             raise Exception("File don't match with any suported")
 
-    def parseEndesa(self, xl, output_name='output_endesa.csv'):
+    def parseEndesa(self, xl, output_name=None):
+        if not output_name:
+            output_name = 'output_endesa.csv'
         df1 = xl.parse(xl.sheet_names[1])
         df_obj = df1.select_dtypes(['object'])
         df1[df_obj.columns] = df_obj.apply(lambda x: x.str.strip())
         df1 = df1.iloc[:-2]
         df1.to_csv(path_or_buf=output_name, sep=';', columns=['Codigo_Fiscal_de_Factura','Importe_Total_de_la_Factura'], header=False, index=False, decimal=',')
 
-    def parseIberdrola(self, xl, output_name='output_iberdrola.csv'):
+    def parseIberdrola(self, xl, output_name=None):
+        if not output_name:
+            output_name='output_iberdrola.csv'
         df1 = xl.parse()
-        df1.to_csv(path_or_buf='output_iberdrola.csv', sep=';', columns=['NUMFACTRUA','IMPORTE EUR'], header=False, index=False, decimal=',')
+        df1.to_csv(path_or_buf=output_name, sep=';', columns=['NUMFACTRUA','IMPORTE EUR'], header=False, index=False, decimal=',')
+
 # vim: et ts=4 sw=4
