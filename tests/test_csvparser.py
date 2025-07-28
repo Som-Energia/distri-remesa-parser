1a00bf7cb4f641ccf5a75f716ac32696442606f8
Oriol Piera <oriol.piera@somenergia.coop>
2025-07-28 09:02:47 +0200

    🎨 Add precommit style

--- tests/test_csvparser.py
+++ tests/test_csvparser.py
@@ -1,13 +1,13 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+from distriremesaparser import csvparser
 import unittest
 
 import csv
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
-from distriremesaparser import csvparser
 
 
 class TestCSVParser(unittest.TestCase):
@@ -21,17 +21,19 @@ class TestCSVParser(unittest.TestCase):
 
     def test__parseUFD__allOk(self):
         csv_file = [['FECHA', 'Nº FACTURA', 'C.U.P.S.', 'IMPORTE'],
-                    ['01/01/2019', 'J419000000000', 'ES0022000000000000NE1P', '23,09'],
-                    ['01/01/2019', 'J419000000001', 'ES0022000000000000NE1P', '53,09'], ] 
+                    ['01/01/2019', 'J419000000000',
+                        'ES0022000000000000NE1P', '23,09'],
+                    ['01/01/2019', 'J419000000001', 'ES0022000000000000NE1P', '53,09'], ]
         p = csvparser.CSVParser()
         p.loadList(csv_file)
 
         result = p.parseUFD()
 
-        self.assertEqual([['J419000000000', '23,09'], ['J419000000001', '53,09']], result)
+        self.assertEqual([['J419000000000', '23,09'], [
+                         'J419000000001', '53,09']], result)
 
     def test__parseERP__allOk(self):
-        csv_file = [["J419000000000","57.91","No trobada"]]
+        csv_file = [["J419000000000", "57.91", "No trobada"]]
         p = csvparser.CSVParser()
         p.loadList(csv_file)
 
@@ -40,11 +42,14 @@ class TestCSVParser(unittest.TestCase):
         self.assertEqual([['J419000000000', '57,91']], result)
 
     def test__procesFile__UFDAllOk(self):
-        csv_file = [['\xef\xbb\xbfRELACI\xc3\x93N DE RECIBOS INCLUIDOS EN UN APUNTE \xc3\x9aNICO para XXXX en fecha 01/01/2019'], [],
-                ['CLIENTE: F00000000-XXXX'], ['SOCIEDAD: 0703-UFD DISTRIBUCI\xc3\x93N ELECTRICIDAD, S.A.'],
-                ['C\xc3\x93DIGO APUNTE \xc3\x9aNICO: 99999'], ['TIPO: REMESAS COBROS'], ['N\xc2\xba DE DOCUMENTOS: 99'],
-                ['IMPORTE TOTAL: 23,09'], [], ['FECHA', 'N\xc2\xba FACTURA', 'C.U.P.S.', 'IMPORTE'],
-                ['01/01/2019', 'J419000000000', 'ES0022000000000000NE1P', '23,09']]
+        csv_file = [
+            ['\xef\xbb\xbfRELACI\xc3\x93N DE RECIBOS INCLUIDOS EN UN APUNTE \xc3\x9aNICO para '
+             'XXXX en fecha 01/01/2019'], [], ['CLIENTE: F00000000-XXXX'],
+            ['SOCIEDAD: 0703-UFD DISTRIBUCI\xc3\x93N ELECTRICIDAD, S.A.'],
+            ['C\xc3\x93DIGO APUNTE \xc3\x9aNICO: 99999'], ['TIPO: REMESAS COBROS'],
+            ['N\xc2\xba DE DOCUMENTOS: 99'], ['IMPORTE TOTAL: 23,09'], [],
+            ['FECHA', 'N\xc2\xba FACTURA', 'C.U.P.S.', 'IMPORTE'],
+            ['01/01/2019', 'J419000000000', 'ES0022000000000000NE1P', '23,09']]
         p = csvparser.CSVParser()
         p.loadList(csv_file)
 
@@ -61,14 +66,17 @@ class TestCSVParser(unittest.TestCase):
         p.procesFile()
 
         result = self.fromCSVtoList('/tmp/output.csv')
-        self.assertEqual([['J419000000000', '23,09'], ['J419000000001', '53,09']], result)
+        self.assertEqual([['J419000000000', '23,09'], [
+                         'J419000000001', '53,09']], result)
 
     def test__procesFile__CSVErr(self):
-        csv_file = [['2019/01/01', 'J419000000000', '23,09'], ['2019/01/01', 'J419000000001', '53,09']]
+        csv_file = [['2019/01/01', 'J419000000000', '23,09'],
+                    ['2019/01/01', 'J419000000001', '53,09']]
         p = csvparser.CSVParser()
         p.loadList(csv_file)
 
-        with self.assertRaises(Exception): p.procesFile()
+        with self.assertRaises(Exception):
+            p.procesFile()
 
 
 if __name__ == '__main__':
--- tests/test_xlsparser.py
+++ tests/test_xlsparser.py
@@ -1,12 +1,12 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+import csv
+from distriremesaparser import xlsparser
 import unittest
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
-from distriremesaparser import xlsparser
-import csv
 
 
 class TestXLSParser(unittest.TestCase):
@@ -18,7 +18,7 @@ class TestXLSParser(unittest.TestCase):
             for row in reader:
                 invoice_list.append(row)
         return invoice_list
-    
+
     def test__procesFile__IberdrolaAllOk(self):
         p = xlsparser.XLSParser()
 
@@ -26,9 +26,9 @@ class TestXLSParser(unittest.TestCase):
 
         result = self.fromCSVtoList('output_iberdrola.csv')
         self.assertEqual([['100000000000000001', '342,82'],
-            ['100000000000000002', '343,82'],
-            ['100000000000000003', '344,82'],
-            ['100000000000000004', '345,82']], result)
+                          ['100000000000000002', '343,82'],
+                          ['100000000000000003', '344,82'],
+                          ['100000000000000004', '345,82']], result)
 
     def test__procesFile__EndesaAllOk(self):
         p = xlsparser.XLSParser()
@@ -37,25 +37,28 @@ class TestXLSParser(unittest.TestCase):
 
         result = self.fromCSVtoList('output_endesa.csv')
         self.assertEqual([['F3901N00000008', '26,24'],
-            ['F3901N00000009', '27,88'],
-            ['F3901N00000010', '21,71'],
-            ['F3901N00000011', '30,25'],
-            ['F3901N00000012', '25,31']], result)
+                          ['F3901N00000009', '27,88'],
+                          ['F3901N00000010', '21,71'],
+                          ['F3901N00000011', '30,25'],
+                          ['F3901N00000012', '25,31']], result)
 
     def test__procesFile__EndesaColumnErr(self):
         p = xlsparser.XLSParser()
 
-        with self.assertRaises(Exception): p.procesFile('./tests/data/test_endesa_err_columname.xlsx')
+        with self.assertRaises(Exception):
+            p.procesFile('./tests/data/test_endesa_err_columname.xlsx')
 
     def test__procesFile__IberdrolaColumnErr(self):
         p = xlsparser.XLSParser()
 
-        with self.assertRaises(Exception): p.procesFile('./tests/data/test_iberdrola_err_columname.xlsx')
+        with self.assertRaises(Exception):
+            p.procesFile('./tests/data/test_iberdrola_err_columname.xlsx')
 
     def test__procesFile__EndesaSheetErr(self):
         p = xlsparser.XLSParser()
 
-        with self.assertRaises(Exception): p.procesFile('./tests/data/test_endesa_err_sheetname.xlsx')
+        with self.assertRaises(Exception):
+            p.procesFile('./tests/data/test_endesa_err_sheetname.xlsx')
 
 
 if __name__ == '__main__':
